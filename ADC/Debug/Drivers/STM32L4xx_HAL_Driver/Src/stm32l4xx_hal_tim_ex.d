27fba2bd61c4ae1a7c14959997f1fee394091752
MaurycyManski <https://MaurycyManski@github.com>
2021-12-14 22:16:11 +0100

    adding code

---
+++ ADC/Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.d
@@ -0,0 +1,107 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Inc/stm32l4xx_hal_conf.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dac.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dac_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Inc/stm32l4xx_hal_conf.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dac.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dac_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+
+C:/Users/Maurycy/Documents/mxcube/ADC/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
